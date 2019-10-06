6123e15305066a6c7ea30b2203ddb05358e04bfd
ZacDair <54358391+ZacDair@users.noreply.github.com>
2019-10-06 19:40:56 +0100

    Update Steam Case Pricer Checker.py
    added some comments, added sleep for 5 seconds to give the user time to see the result, and changed the rounding to 3

--- Steam Case Price Checker/Steam Case Pricer Checker.py
+++ Steam Case Price Checker/Steam Case Pricer Checker.py
@@ -1,12 +1,13 @@
 import urllib.request
 from datetime import date
+import time
 # Steam Date Format = Oct 03 2019
 # Datetime Format = 2019-10-03
 
 todayDate = (date.today())
 url = "https://steamcommunity.com/market/listings/730/eSports%202014%20Summer%20Case"
 
-
+# Get actual date and change it to the format Steam uses
 def getCalenderDate(x):
     months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
     dateParts = str(x).split("-")
@@ -20,7 +21,7 @@ def getCalenderDate(x):
 
     return newDate
 
-
+# Get todays date, and run getCalenderDate, open our url, split content by date
 date = getCalenderDate(todayDate)
 with urllib.request.urlopen(url) as url:
     contents = url.read()
@@ -33,10 +34,12 @@ while i < len(res)-1:
     temp = (res[i].split(","))
     prices.append(temp[1])
     i = i + 1
+
+# Get average price
 avg = 0
 for p in prices:
     avg = avg + float(p)
 avg = avg / len(prices)
 
-print("The average for " + date + " is " + str(round(avg, 2)) + "$")
-
+print("The average for " + date + " is " + str(round(avg, 3)) + "$")
+time.sleep(5)
